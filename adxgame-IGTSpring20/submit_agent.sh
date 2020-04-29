48c3f69ea58e0ffd154d63343afd94cb804552cb
ishita vohra <ishitavohra3110@gmail.com>
2020-04-30 01:19:04 +0530

    uncomment

--- adxgame-IGTSpring20/submit_agent.sh
+++ adxgame-IGTSpring20/submit_agent.sh
@@ -1,22 +1,22 @@
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.FourAgent -illegal-access=warn &
-# echo "Started AgentFour"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.OneAgent -illegal-access=warn &	
-# echo "Started AgentOne"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.TwoAgent -illegal-access=warn &
-# echo "Started AgentTwo"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.ThreeAgent -illegal-access=warn &	
-# echo "Started AgentThree"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.FifthAgent -illegal-access=warn &
-# echo "Started AgentFive"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.SixthAgent -illegal-access=warn &	
-# echo "Started AgentSix"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.SeventhAgent -illegal-access=warn &
-# echo "Started AgentSeven"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.FourAgent -illegal-access=warn &
+echo "Started AgentFour"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.OneAgent -illegal-access=warn &	
+echo "Started AgentOne"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.TwoAgent -illegal-access=warn &
+echo "Started AgentTwo"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.ThreeAgent -illegal-access=warn &	
+echo "Started AgentThree"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.FifthAgent -illegal-access=warn &
+echo "Started AgentFive"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.SixthAgent -illegal-access=warn &	
+echo "Started AgentSix"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.SeventhAgent -illegal-access=warn &
+echo "Started AgentSeven"
 java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.EightAgent -illegal-access=warn &	
 echo "Started AgentEight"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.NinthAgent -illegal-access=warn &
-# echo "Started AgentNine"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.TenthAgent -illegal-access=warn &	
-# echo "Started AgentTen"
-# java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.EleventhAgent -illegal-access=warn &
-# echo "Started AgentEleven"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.NinthAgent -illegal-access=warn &
+echo "Started AgentNine"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.TenthAgent -illegal-access=warn &	
+echo "Started AgentTen"
+java -cp "$(pwd)/dependencies:commons-math3-3.0.jar:$(pwd)/dependencies/cplex.jar:$(pwd)/dependencies/guava-19.0.jar:$(pwd)/dependencies/kryonet-2.21-all.jar:$(pwd)/dependencies/adx.jar:./:$(pwd)/src/:$(pwd)/bin/" adx.variants.thirtydaysgame.EleventhAgent -illegal-access=warn &
+echo "Started AgentEleven"
